3dc6dcc078e2afa6ab348ac9f864827226eccb39
Bettina Lechner <ergdegdeg@gmx.com>
2015-07-03 18:05:44 -0500

    minimal angular app

---
+++ app/assets/javascripts/app.coffee
@@ -0,0 +1,1 @@
+moviedb = angular.module('moviedb', []);
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module MoviedbAngular
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'bootstrap-sass-official', 'assets', 'fonts')
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
+    config.assets.precompile += %w( bootstrap-sass-official/assets/fonts/bootstrap/glyphicons-halflings-regular.woff2 )
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
