b807cf5cfdd67a9492b121d1c76875812209343f
Bettina Lechner <ergdegdeg@gmx.com>
2015-07-04 17:18:54 -0500

    create movies before feature tests

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper.rb'
 
 feature 'Looking up movies', js: true do
+	before do
+		Movie.create!({ title: 'Pulp Fiction', year: '1994', rating: 9 })
+		Movie.create!({ title: 'Kill Bill: Vol. 1', year: '2003', rating: 8 })
+		Movie.create!({ title: 'Kill Bill: Vol. 2', year: '2004', rating: 8 })
+	end
+
 	scenario 'finding movies' do
 		visit '/'
-		fill_in 'keywords', with: 'pulp'
+		fill_in 'keywords', with: 'kill'
 		click_on 'Search'
 
-		expect(page).to have_content('Pulp Fiction')
+		expect(page).to have_content('Kill Bill')
 	end
 end
